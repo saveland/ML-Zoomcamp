8cefc8d7a5b537308e98731b6755f9386c480243
Paolo Salvatierra <psalvatierra@outlook.com>
2024-09-29 01:53:21 -0700

    Q6 done

--- homework1.py
+++ homework1.py
@@ -22,3 +22,19 @@ print(missing)
 # Q5. Maximum final price
 Dell_max_final_price = df[df['Brand'] == 'Dell']['Final Price'].max()
 print(Dell_max_final_price)
+
+# Q6. Median value of Screen
+# 1. Find the median value of Screen column in the dataset.
+Screen_median_value = df['Screen'].median()
+print(Screen_median_value)
+
+#2. Next, calculate the most frequent value of the same Screen column.
+Screen_mode_value = df['Screen'].mode()
+print(Screen_mode_value)
+
+# 3. Use fillna method to fill the missing values in Screen column with the most frequent value from the previous step.
+df['Screen'].fillna(df['Screen'].mode()[0], inplace=True)
+
+# # 4. Now, calculate the median value of Screen once again.
+Screen_median_value = df['Screen'].median()
+print(Screen_median_value)
